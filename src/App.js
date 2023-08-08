d82984d5690646312013c4f8ac0a2f82a7c1ecb6
Euphoria99 <363pavanbhat@gmail.com>
2023-08-08 22:55:49 +0530

    JSX, Components and  Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <h2>Hii</h2>
+          <User name='pavan' age={24} email="pavan@gmail.com" />
+          <User name='alice' age={25} email="alice@gmail.com"/>
     </div>
   );
 }
 
+
+// defining a user component
+const User = (props) => {
+
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  )
+}
 export default App;
