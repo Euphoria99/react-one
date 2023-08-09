6a13f10d7d570b48929b56e310f6f3f9e9c3f0a1
Euphoria99 <363pavanbhat@gmail.com>
2023-08-09 22:01:14 +0530

    Basic incrementor using useState hook

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
 import './App.css';
-import {User} from './User.js'
-import {PlanetsCp} from './Planets.js'
+
+import { useState } from "react";
 
 function App() {
 
-  const age = 17;
+  const [age, setAge] = useState(0);
 
-  const isGreen = true;
-
-  const users = [
-    { name: "Pavan", age: 24 },
-    { name: "Jake", age: 25 },
-    { name: "Jessica", age: 45 },
-    ];
-
-  
-    //assignment
-    const planets = [
-      { name: "Mars", isGasPlanet: false },
-      { name: "Earth", isGasPlanet: false },
-      { name: "Jupiter", isGasPlanet: true },
-      { name: "Venus", isGasPlanet: false },
-      { name: "Neptune", isGasPlanet: true },
-      { name: "Uranus", isGasPlanet: true },
-      ];
-
-
+  const increaseAge = () => {
+    setAge(age + 1)
+  }
   return (
-  
-    //ternary operators 
   <div className='App'>
-    {age >= 18 ? <h1>OVERAGE</h1> : <h1>UNDERAGE</h1>}
-    <h1 style={{color: isGreen ? "green" : "red"}}>THIS HAS COLOR</h1>
-
-    {isGreen && <button>Submit</button>}
-
-
-    {/* Lists */}
-    
-    {users.map( (user,key) => {
-      return <User name={user.name} age={user.age} />
-    })}
-
-    {/* Assignment */}
-    
-    {
-      planets.map( (planet,key) => {
-        return <PlanetsCp name={planet.name} isGasPlanet={planet.isGasPlanet} /> 
-      })
-  
-    }
-
+    {age} 
+    <button onClick={increaseAge}>Increase</button>
   </div>
-
-
-
   );
 }
 
