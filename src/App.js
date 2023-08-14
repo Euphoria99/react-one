77c49f30f04d4ea74fbabe11d47a6fbe42d521d6
Euphoria99 <363pavanbhat@gmail.com>
2023-08-14 23:42:14 +0530

    Fetching data from API using axios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
-import { useState } from "react";
-import { Text } from './Text.js'
+import Axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+  
+  // fetch("https://catfact.ninja/fact").then(
+  //   (res) => res.json()
+  // ).then(
+  //   (data) => console.log('The Data', data)
+  // )
 
-  const[showText, setShowText] = useState(false)
+  const [catFact,setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then( (res) => {
+      console.log('The Data',res.data)
+      setCatFact(res.data.fact)
+    })
+  }
+
+  useEffect( () => {
+    fetchCatFact()
+  }, []);
+
 
   return (
   <div className='App'>
-    <button 
-    onClick={
-      ()=>{
-        setShowText(!showText)
-      }
-    }>Show Text</button>
-   { showText && <Text />}
+    <button onClick={fetchCatFact}>Generate a Cat Fact</button>
+   <p> {catFact} </p>
   </div>
   );
 }
