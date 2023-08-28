f4255dc4d14fc9b51a3b27c14dffd9418d144a47
Euphoria99 <363pavanbhat@gmail.com>
2023-08-28 21:58:22 +0530

    Counter App made with custom hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
-import {Cat } from "./Cat"
-import { QueryClient, QueryClientProvider} from "@tanstack/react-query"
-
+import { useCount } from "./useCount";
 
 
 function App() {
 
-  const client = new QueryClient({defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false
-    }
-  }});
-
+  const {count, increase, decrease, reset} = useCount();
   return (
   <div className='App'>
-    <QueryClientProvider client={client}>
-      <Cat />
-    </QueryClientProvider>
+    {count}
+    <button onClick={increase}>Increase</button>
+    <button onClick={decrease}>Decrease</button>
+    <button onClick={reset}>Reset</button>
   </div>
   );
 }
