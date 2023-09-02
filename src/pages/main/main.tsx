167e1002b1b3c1df2cbcefee2d526c496cd8a303
Euphoria99 <363pavanbhat@gmail.com>
2023-09-02 23:59:50 +0530

    Firebase project part 3

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,39 @@
+import { getDocs, collection}  from "firebase/firestore"
+import { db } from "../../config/firebase"
+import { useEffect, useState } from "react";
+import { Post } from "./post";
+
+export interface Post {
+    id:string,
+    description:string,
+    title:string,
+    userId:string,
+    username: string
+}
+
+export const Main = () => {
+
+    const [postsList, setPostsList] = useState<Post[] | null>(null);
+    
+    const postsRef = collection(db, "posts")
+
+    const getPosts = async () => {
+         const data = await getDocs(postsRef);
+         console.log("Fetched data:", data.docs); 
+         setPostsList(
+            data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))  as Post[]
+         )
+    }
+
+    useEffect( () => {
+
+        getPosts();
+    }, [])
+    return(
+        <div>
+            {postsList?.map( (post) => (
+                <Post key={post.id} post={post}/>
+            ))}
+        </div>
+    )
+}
---
+++ src/pages/main/post.tsx
@@ -0,0 +1,28 @@
+import { Post as IPost } from "./main"
+
+interface Props {
+    post: IPost;
+}
+
+
+export const Post = (props: Props) => {
+
+    const { post } = props;
+
+    return (
+        <div className="container">
+
+        <div className="card">
+            <div className="titleCard">
+                <h1> {post.title} </h1>
+            </div>
+            <div className="bodyCard">
+                <p> {post.description} </p>
+            </div>
+            <div className="footerCard">
+                <p> @ {post.username} </p>
+            </div>
+        </div>
+    </div>
+    )
+}
